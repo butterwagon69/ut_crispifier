93e298a339d811e979642be0884967987c7eef7f
butterwagon69 <butterwagon69@gmail.com>
2021-09-05 08:13:33 -0700

    Update schema

--- schema.sql
+++ schema.sql
@@ -8,7 +8,6 @@ CREATE TABLE inputs (
 CREATE TABLE models (
     model_hash string NOT NULL,
     model_name string NOT NULL,
-    model_data blob NOT NULL,
     PRIMARY KEY (model_hash)
 );
 
@@ -28,3 +27,22 @@ CREATE TABLE preferences (
     FOREIGN KEY (image_hash, model_hash) REFERENCES outputs(image_hash, model_hash)
 );
 
+CREATE TABLE IF NOT EXISTS palettes (
+    palette_hash string NOT NULL,
+    palette_data blob NOT NULL,
+    PRIMARY KEY (palette_hash)
+);
+CREATE TABLE IF NOT EXISTS mipmaps (
+    image_hash string NOT NULL,
+    mipmap_index integer NOT NULL,
+    mipmap_data blob NOT NULL,
+    PRIMARY KEY (image_hash, mipmap_index),
+    FOREIGN KEY (image_hash) REFERENCES inputs(image_hash)
+);
+CREATE TABLE IF NOT EXISTS palette_uses(
+    palette_hash string NOT NULL,
+    image_hash string NOT NULL,
+    PRIMARY KEY (palette_hash, image_hash),
+    FOREIGN KEY (image_hash) REFERENCES inputs(image_hash),
+    FOREIGN KEY (palette_hash) REFERENCES palettes(palette_hash)
+);
